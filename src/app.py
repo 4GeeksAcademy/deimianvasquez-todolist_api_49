1513edc998d93eecf4884123507aa158ab090d56
Deimian Vásquez <deimianvasquez@gmail.com>
2025-06-05 01:33:40 +0000

    add endpoints

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Todos
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,167 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route("/health-check", methods=["GET"])
+def health_check():
+    return jsonify("ok"), 200
 
-    return jsonify(response_body), 200
+
+@app.route('/user', methods=['POST'])
+def add_user():
+    body = request.json
+
+    if body.get("username") is None:
+        return jsonify("Se debe enviar la llave username"), 400
+
+    if body.get("email") is None:
+        return jsonify("Se debe enviar la llave email"), 400
+
+    # User.username="", User.email=""
+    user = User.query.filter_by(
+        username=body["username"], email=body["email"]).first()
+
+    if user is not None:
+        user = user.serialize()
+
+        if user.get("username") is not None or user.get("email") is not None:
+            return jsonify(f"El username {body["username"]} ya se encuentra registrado")
+
+    user = User()
+    user.username = body.get("username").lower()
+    user.email = body["email"]
+
+    db.session.add(user)
+
+    try:
+        db.session.commit()
+        return jsonify("Usuario registrado exitosamente"), 201
+    except Exception as error:
+        db.session.rollback()
+        return jsonify(f"Error: {error.args}"), 500
+
+
+@app.route("/user", methods=["GET"])
+def get_all_users():
+    users = User.query.all()
+
+    # HEcho con for
+    # response_body = []
+    # for item in users:
+    #     response_body.append(item.serialize())
+
+    # hecho con map
+    # response_body = list(map(lambda item: item.serialize(), users))
+
+    # List Comprehension
+    # response_body = [item.serialize() for item in users]
+
+    return jsonify([item.serialize() for item in users]), 200
+
+
+@app.route("/user/<int:the_id>", methods=["GET"])
+def get_one_user(the_id):
+    user = User.query.get(the_id)
+
+    if user is None:
+        return jsonify("El usuario no esta registrado"), 404
+
+    return jsonify(user.serialize())
+
+
+@app.route("/todos", methods=["POST"])
+def add_todo():
+    # 1.- Traernos el body del request
+    data = request.json
+    # 2.- Validar que si sea una tarea
+    if data.get("label") is None:
+        return jsonify("Debes enviar la llave label"), 400
+    # 3.- Creamos el molde, esta es la instancia de la clase Todos
+    todo = Todos()
+    # 4.- Rellenar el molde
+    todo.label = data["label"]
+    todo.user_id = data["user_id"]
+    # 5.- Agregarla a la session de base de datos
+    db.session.add(todo)
+    # 6.- Agregar el commit del cambio perooooo dentro de un try except
+
+    try:
+        db.session.commit()
+        return jsonify("tarea registrada exitosamente"), 201
+    except Exception as error:
+        db.session.rollback()
+        return jsonify(f"Error: {error.args}")
+
+
+@app.route("/todos/<int:the_id>", methods=["PUT"])
+def update_task(the_id=None):
+    body = request.json
+
+    todo = Todos.query.get(the_id)
+
+    if body.get("is_done") is None:
+        return jsonify("debes enviar una llave id_done"), 400
+
+    if body.get("label") is None:
+        return jsonify("debes enviar una llave id_done"), 400
+
+    if todo is None:
+        return jsonify("User not found"), 404
+
+    todo.is_done = body["is_done"]
+    todo.label = body["label"]
+
+    try:
+        db.session.commit()
+        return jsonify(todo.serialize()), 201
+    except Exception as error:
+        db.session.rollback()
+        return jsonify(f"Error: {error.args}")
+
+
+@app.route("/todos/<int:the_id>",  methods=["DELETE"])
+def delete_task(the_id=None):
+    todo = Todos.query.get(the_id)
+
+    if todo is None:
+        return jsonify("User not found"), 404
+
+    db.session.delete(todo)
+    try:
+        db.session.commit()
+        return jsonify([]), 204
+    except Exception as error:
+        return jsonify(f"Error: {error.args}")
+
+
+@app.route("/user/<int:the_id>", methods=["DELETE"])
+def delete_user(the_id=None):
+    user = User.query.get(the_id)
+
+    if user is None:
+        return jsonify("User not found"), 404
+
+    db.session.delete(user)
+
+    try:
+        db.session.commit()
+        return jsonify([]), 204
+    except Exception as error:
+        return jsonify(f"Error: {error.args}")
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,64 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, DateTime, func, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+# En la base de datos no se guarda las imagenes, se guarda la url que referencia a esa imagen
+class User(db.Model):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True)
+    email: Mapped[str] = mapped_column(String(50), unique=True)
+    avatar: Mapped[str] = mapped_column(String(
+        180), nullable=False, default="https://randomuser.me/api/portraits/women/41.jpg")
+    create_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now())
+    update_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
+
+    todos: Mapped[list["Todos"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan")
+
+    # Método de serializado --> Pasa la clase de su tipo a un diccionario
+    # podemos tener cuantos serializados necesitemos
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "avatar": self.avatar,
+            # "todos": [item.serialize() for item in self.todos] --> list complehensions
+            "todos": list(map(lambda item: item.serialize(), self.todos))
+            # OJO, LA PASSWORD NO SE SERIALIZA
+        }
+
+
+class Todos(db.Model):
+    __tablename__ = "todos"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    label: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_done: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False)
+    create_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now())
+    update_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"))  # llave foranea
+
+    user: Mapped["User"] = relationship(
+        back_populates="todos")
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "label": self.label,
+            "is_done": self.is_done,
+            "user_id": self.user_id
         }
